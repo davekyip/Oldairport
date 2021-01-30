102126fa5beba947e5f8dbc7788e0197e7bd99e8
davekyip <78003702+davekyip@users.noreply.github.com>
2021-01-30 17:43:20 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,305 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-top-view",
+      "name": "Top View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6317496517526706,
+          "pitch": 0.10510723257304377,
+          "rotation": 0,
+          "target": "1-middle-view"
+        },
+        {
+          "yaw": -2.7270481905782162,
+          "pitch": -0.04298908431135118,
+          "rotation": 0,
+          "target": "4-old-foundation"
+        },
+        {
+          "yaw": -2.443049821123145,
+          "pitch": -0.026021907198437333,
+          "rotation": 0,
+          "target": "5-model"
+        },
+        {
+          "yaw": -1.937898062770195,
+          "pitch": -0.03230203895313721,
+          "rotation": 0,
+          "target": "2-round-door"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-middle-view",
+      "name": "Middle view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23675070043358026,
+          "pitch": -0.0936301508418893,
+          "rotation": 0,
+          "target": "4-old-foundation"
+        },
+        {
+          "yaw": 0.7063034432610245,
+          "pitch": -0.016534144949151397,
+          "rotation": 0,
+          "target": "5-model"
+        },
+        {
+          "yaw": 2.254271878220652,
+          "pitch": -0.16333918217553212,
+          "rotation": 0,
+          "target": "0-top-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-round-door",
+      "name": "Round door",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.271769589456614,
+          "pitch": -0.1905090427744085,
+          "rotation": 0,
+          "target": "0-top-view"
+        },
+        {
+          "yaw": -1.2310781319173394,
+          "pitch": -0.058889669133222355,
+          "rotation": 0,
+          "target": "1-middle-view"
+        },
+        {
+          "yaw": 2.856635948658746,
+          "pitch": -0.13354438675316693,
+          "rotation": 0,
+          "target": "3-water-fountain"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-water-fountain",
+      "name": "Water Fountain",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8326337367313252,
+          "pitch": -0.1843499850124548,
+          "rotation": 0,
+          "target": "2-round-door"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-old-foundation",
+      "name": "Old Foundation",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3292867287762995,
+          "pitch": -0.022668926003717615,
+          "rotation": 0,
+          "target": "5-model"
+        },
+        {
+          "yaw": -2.135803779788837,
+          "pitch": -0.20162150837476744,
+          "rotation": 0,
+          "target": "1-middle-view"
+        },
+        {
+          "yaw": 2.717659785815762,
+          "pitch": -0.3312331941637616,
+          "rotation": 0,
+          "target": "0-top-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-model",
+      "name": "Model",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8494103637489987,
+          "pitch": -0.05051969074252405,
+          "rotation": 0,
+          "target": "4-old-foundation"
+        },
+        {
+          "yaw": 1.3452753013807826,
+          "pitch": -0.11261425039019457,
+          "rotation": 0,
+          "target": "1-middle-view"
+        },
+        {
+          "yaw": 0.43532798352775437,
+          "pitch": -0.27089954898179514,
+          "rotation": 0,
+          "target": "0-top-view"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Kowloon Walled City 1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
